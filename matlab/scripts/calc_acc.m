59047691415959b553020ed59fecfbd494ad20d8
AndreasHolm <aholm99@hotmail.com>
2016-03-01 20:21:39 +0100

    fixed minor issue

--- matlab/scripts/calc_acc.m
+++ matlab/scripts/calc_acc.m
@@ -5,18 +5,24 @@ tlk = transpose(lk);
 if pick_kernel == 1
     h_WL = 0;
     K_WL = WL(mutag, h_WL, 0);
-    result = runntimes(K_WL{1,h_WL + 1}, tlmutag, n);
+    result = runntimes(K_WL{1,h_WL + 1}, tlk, n);
 elseif pick_kernel == 2
     h_RG = 0;
     K_RG = RGkernel(mutag, h_RG);
-    result = runntimes(K_RG, tlmutag, n);
+    result = runntimes(K_RG, tlk, n);
 elseif pick_kernel == 3 
     K_l3 = l3graphletkernel(mutag);
-    result = runntimes(K_l3, tlmutag, n);
-%{
+    result = runntimes(K_l3, tlk, n);
 elseif pick_kernel == 4
-    lRWkernel(graph, lambda, size{1 = small, 0 = otherwise})
-%}
+    cols = size(graph,2);
+    maxDegree = 0;
+    degrees = cell(1,cols);
+    for col = 1:cols
+       degrees{col} = sum(full(graph(col).('am')),2);
+       maxDegree = max([maxDegree, max(degrees{col})]);
+    end
+    K_RW = lRWkernel(graph, 10^(1/maxDegree^2), 0);
+    result = runntimes(K_RW, tlk, n);
 elseif pick_kernel == 5
     K_sp = spkernel(graph, 0);
     result = runntimes(K_sp, tlk, n);
