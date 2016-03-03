f0b86d412d4c10c12f9c395157b8983b6cf9093c
AndreasHolm <aholm99@hotmail.com>
2016-03-03 11:48:41 +0100

    Added graphhopper

--- matlab/scripts/calc_acc.m
+++ matlab/scripts/calc_acc.m
@@ -3,11 +3,11 @@ function result = calc_acc(graph,lk,n,pick_kernel)
 tlk = transpose(lk);
 
 if pick_kernel == 1
-    h_WL = 7; %Number of iterations.
+    h_WL = 10; %Number of iterations.
     K_WL = WL(graph, h_WL, 0);
     result = runntimes(K_WL{1,h_WL + 1}, tlk, n);
 elseif pick_kernel == 2
-    h_RG = 0; %Maximal tree height.
+    h_RG = 3; %Maximal tree height.
     K_RG = RGkernel(graph, h_RG);
     result = runntimes(K_RG, tlk, n);
 elseif pick_kernel == 3 
@@ -26,6 +26,9 @@ elseif pick_kernel == 4
 elseif pick_kernel == 5
     K_sp = spkernel(graph, 0);
     result = runntimes(K_sp, tlk, n);
+elseif pick_kernel == 6
+    K_GH = GraphHopper_dataset(graph, 'dirac', 0, 0);
+    result = runntimes(K_GH, tlk, n);
 else
     error('Pick Kernel');
 end
