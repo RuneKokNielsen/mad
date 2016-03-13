33fee486cfdf524bee016cddfaa2b6c8e0e059be
Rune Kok Nielsen <rune@runekoknielsen.dk>
2016-03-13 14:12:45 +0100

    Ny sigma optimizer base

---
+++ matlab/scripts/opt_baselinev2.m
@@ -0,0 +1,11 @@
+function result = opt_baselinev2(funKernel, graphs, labels)
+
+labels = transpose(labels);
+
+wValues = [0.1, 0.5, 1, 5, 10, 25, 50, 100, 200, 500, 1000];
+
+multiK = arrayfun(@ (x) (feval(funKernel, graphs, x)), wValues, 'un',0);
+
+result = runntimes_multi(multiK, labels, 10);
+
+end
