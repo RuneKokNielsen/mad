2b0e5dbfe2afa774339fdd8e5ef8eabf79b723a2
Rune Kok Nielsen <rune@runekoknielsen.dk>
2016-03-13 15:57:01 +0100

    Cross validation gaussian optimization

--- matlab/scripts/opt_baselinev2.m
+++ matlab/scripts/opt_baselinev2.m
@@ -2,7 +2,8 @@ function result = opt_baselinev2(funKernel, graphs, labels)
 
 labels = transpose(labels);
 
-wValues = [0.1, 0.5, 1, 5, 10, 25, 50, 100, 200, 500, 1000];
+wValues = [0.1, 0.5, 1, 5, 10, 25, 50, 75, 100, 200, 500, 750, 1000];
+
 
 multiK = arrayfun(@ (x) (feval(funKernel, graphs, x)), wValues, 'un',0);
 
